48ad644d3ec7fa5a1d0ca200568b4a35926682fe
Yulia <122625932+Uya13@users.noreply.github.com>
2023-11-23 22:12:25 +0300

    views has been added urls has been added

--- pereval/pereval/urls.py
+++ pereval/pereval/urls.py
@@ -15,8 +15,20 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+from rest_framework import routers
+from app import views
+
+router = routers.DefaultRouter()
+router.register(r'users', views.UsersViewset)
+router.register(r'coords', views.CoordsViewset)
+router.register(r'level', views.LevelViewset)
+router.register(r'perevals', views.PerevalsViewset)
+router.register(r'images', views.ImagesViewset)
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('submitData/', include('rest_framework.urls', namespace='rest_framework')),
+    path('', include(router.urls))
 ]
